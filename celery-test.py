d3bcf1e2bb0f8cc4ca853bd503c2dc2d6e85c676
Pradyumna Sridhara <prsridha@ucsd.edu>
2022-05-23 18:07:10 -0600

    more nearter code

--- celery-test.py
+++ celery-test.py
@@ -29,43 +29,56 @@ celery = make_celery(app)
 def health():
     return "Yes this is celery test working"
 
-@celery.task(name="create_task")
-def create_task(n):
-    print("Got request for n = ", n)
-    sleep(n)
-    return True
+@celery.task(name="train_model")
+def train_model(model_id, epoch_num):
+    print("Got request for model_id = ", model_id, "and epoch_num = ", epoch_num)
+    sleep(5)
+    data = {
+        "model_id": model_id,
+        "epoch_num": epoch_num + 1
+    }
+    return data
 
-@app.route("/create-task/<n>")
-def run_task(n):
-    # n = request.args.get('n')
-    task = create_task.delay(int(n))
-    return jsonify({"task_id": task.id}), 202
+@app.route("/run-subepoch", methods=['POST'])
+def run_subepoch():
+    content = request.json
+    model_id = int(content["model_id"])
+    epoch_num = int(content["epoch_num"])
 
-@app.route("/tasks/<task_id>", methods=["GET"])
-def get_status(task_id):
-    print("GOT GET STATUS TASK ID", task_id)
+    model_building_task = train_model.delay(model_id, epoch_num)
+
+    return jsonify({"task_id": model_building_task.id}), 202
+
+@app.route("/check-status/<task_id>", methods=["GET"])
+def check_status(task_id):
     task_result = celery.AsyncResult(task_id)
-    print("NEW_STUFF", task_result)
     result = {
         "task_id": task_id,
         "task_status": task_result.status,
         "task_result": task_result.result
     }
-    print("RESULT", json.dumps(result))
     return jsonify(result), 200
 
 @app.route("/send")
 def send_req():
-    response = requests.get('http://localhost:9998/create-task/2')
+    post_data = {
+        "model_id": 10,
+        "epoch_num": 20
+    }
+    url = "http://localhost:9998/run-subepoch"
+    headers = {'Content-type': 'application/json'}
+    response = requests.post(url, data=json.dumps(post_data), headers=headers)
     content = json.loads(response.content)
     task_id = content["task_id"]
     print("GOT TASK ID: ", task_id)
 
     while True:
-        response = requests.get("http://localhost:9998/tasks/{}".format(task_id))
+        response = requests.get("http://localhost:9998/check-status/{}".format(task_id))
         content = json.loads(response.content)
         pprint(content)
+        sleep(1)
         if content["task_status"] == "SUCCESS":
+            print(content["task_result"])
             break
     return "done"
 
